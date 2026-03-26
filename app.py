1bddbe1c6753b0e8e7fe1f2e0b3adc2b5d763d22
deloaiprivatelimited <deloaiprivatelimited@gmail.com>
2026-03-26 11:27:44 +0530

    deploy commit

--- app.py
+++ app.py
@@ -59,4 +59,5 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.getenv("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
